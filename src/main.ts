3f1eca4bb044f9dcceb2fe42806b2c2d5bc0cd26
Evan Chisholm <elchisholm248@gmail.com>
2022-11-09 07:45:02 -0500

    setup board

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,45 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.log("hello world");
+const boardElement = document.querySelector("#board");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    const square = document.createElement("div");
+    square.classList.add((i + j) % 2 == 0 ? "white" : "black");
+    square.classList.add(`${i}:${j}`);
+    square.classList.add("square");
+    boardElement?.appendChild(square);
+  }
+}
+
+class Board {
+  pieces: Piece[];
+
+  constructor() {
+    this.pieces = [];
+  }
+}
+
+enum pieceType {
+  king,
+  queen,
+  rook,
+  bishop,
+  knight,
+  pawn,
+}
+
+class Piece {
+  x: number;
+  y: number;
+  color: "black" | "white";
+  type: pieceType;
+
+  constructor(type: pieceType, color: "white" | "black", x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.type = type;
+  }
+}
