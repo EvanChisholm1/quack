182a483bf5a72d7dffee5cbdd057ead8f27aa7a4
Evan Chisholm <59626978+EvanChisholm1@users.noreply.github.com>
2022-11-09 11:26:43 -0500

    added initial board setup I hope, I wrote this with no way to see the board setup or the actual sitw running

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,40 @@ class Board {
 
   constructor() {
     this.pieces = [];
+
+    // pawns
+    for(let i = 0; i < 8; i++) {
+      this.pieces.push(new Piece(pieceType.pawn, "black", 1, i))
+      this.pieces.push(new Piece(pieceType.pawn, "white", 6, i))
+    }
+
+    // rooks
+    this.pieces.push(new Piece(pieceType.rook, "black", 0, 0))
+    this.pieces.push(new Piece(pieceType.rook, "black", 7, 0))
+
+    this.pieces.push(new Piece(pieceType.rook, "white", 0, 7))
+    this.pieces.push(new Piece(pieceType.rook, "white", 7, 7))
+
+    // bishops
+    this.pieces.push(new Piece(pieceType.bishop, "black", 2, 0))
+    this.pieces.push(new Piece(pieceType.bishop, "black", 5 , 0))
+
+    this.pieces.push(new Piece(pieceType.bishop, "white", 2, 7))
+    this.pieces.push(new Piece(pieceType.bishop, "white", 5, 7))
+
+    // knight
+    this.pieces.push(new Piece(pieceType.knight, "black", 1, 0))
+    this.pieces.push(new Piece(pieceType.knight, "black", 6, 0))
+
+    this.pieces.push(new Piece(pieceType.knight, "white", 1, 7))
+    this.pieces.push(new Piece(pieceType.knight, "white", 6, 7))
+
+    // kings
+    this.pieces.push(new Piece(pieceType.king, "black", 4, 0))
+    this.pieces.push(new Piece(pieceType.king, "white", 4, 7))
+
+    this.pieces.push(new Piece(pieceType.queen, "black", 3, 0))
+    this.pieces.push(new Piece(pieceType.queen, "white", 3, 7))
   }
 }
 
@@ -31,14 +65,12 @@ enum pieceType {
 }
 
 class Piece {
-  x: number;
-  y: number;
+  positions: Array<{x: number, y: number}>
   color: "black" | "white";
   type: pieceType;
 
   constructor(type: pieceType, color: "white" | "black", x: number, y: number) {
-    this.x = x;
-    this.y = y;
+    this.positions = [{x, y}];
     this.color = color;
     this.type = type;
   }
